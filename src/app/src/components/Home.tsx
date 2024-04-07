b9bf89111ba3924c96911cc0bc51ab056b515a24
svylabs <sg@svylabs.com>
2024-04-07 13:12:47 +0200

    Updating tagline

--- src/app/src/components/Home.tsx
+++ src/app/src/components/Home.tsx
@@ -397,7 +397,7 @@ const Home: React.FC = () => {
               <h2 className="flex flex-col py-2 text-2xl md:text-3xl lg:text-5xl font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-pink-500">
                 Microcraft
                 <span className="text-xs md:text-3sm lg:text-base font-light text-transparent">
-                Small is Beautiful: Tiny apps to Rescue Time
+                Ad free tools: minimum distraction, maximum utility
                 </span>
               </h2>
             </div>
