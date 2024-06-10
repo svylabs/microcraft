ab139776b1d2bfbb32c70e6125f3fe5807260b9b
svylabs <sg@svylabs.com>
2024-06-10 18:20:27 +0200

    Adding APIs to register contracts

--- src/lib/routes/contract-registry/index.ts
+++ src/lib/routes/contract-registry/index.ts
@@ -21,35 +21,44 @@ class Contract {
     id: string | undefined;
     name: string | undefined;
     description: string | undefined;
-    latest_version: string | undefined;
     type: string | undefined; // cosmos, cosmwasm, eth, solana, near etc...
-    owner: string | undefined;
-    created_at: Date | undefined;
-    updated_at: Date | undefined;
+    latest_version?: string;
+    team?: string;
+    created_at?: Date;
+    updated_at?: Date;
+    constructor(id: string, name: string, description: string, type: string, team: string, version: string="v1") {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.type = type;
+        this.team = team;
+        this.created_at = new Date();
+        this.updated_at = new Date();
+    }
 }
 
 class ContractVersion {
     contract_id: string | undefined;
     version: string | undefined;
-    properties: any | undefined; // ABI, protobuf, etc.
-    created_at: Date | undefined;
-    updated_at: Date | undefined;
+    properties: any; // ABI, protobuf, etc.
+    created_at?: Date;
+    updated_at?: Date;
 }
 
 class ContractInstance {
     contract_id: string | undefined;
     version: string | undefined;
-    address: string | undefined;
-    network: any | undefined;
-    created_at: Date | undefined;
-    updated_at: Date | undefined;
+    deployed_address: string | undefined;
+    network: any | undefined; // eth_mainnet, eth_sepollia, etc..
+    created_at?: Date;
+    updated_at?: Date;
 }
 
 contractRegistryRouter.get("/list", authenticatedUser, async (req: Request, res: Response) => {
     const datastore = getDatastore();
     const session = req.session as CustomSession;
     const query = datastore.createQuery(CONTRACT_TABLE)
-        .filter("owner", "IN", session?.user?.teams)
+        .filter("team", "IN", session?.user?.teams)
     const [contracts] = await datastore.runQuery(query);
     res.json(contracts);
 });
@@ -83,30 +92,126 @@ contractRegistryRouter.get("/get/:id", authenticatedUser, async (req: Request, r
 
 contractRegistryRouter.put("/new", authenticatedUser, async (req: Request, res: Response) => {
     const datastore = getDatastore();
-    const key = datastore.key("ContractRegistry");
-    const contract = {
+    const contract_id = mcutils.getId(req.body.name);
+    const key = datastore.key([CONTRACT_TABLE, contract_id]);
+    const contract = new Contract(contract_id, req.body.name, req.body.description, req.body.type, req.body.team);
+    const entity = {
         key,
-        data: req.body,
+        data: [
+            {
+                name: "name",
+                value: contract.name,
+            },
+            {
+                name: "description",
+                value: contract.description,
+                excludeFromIndexes: true,
+            },
+            {
+                name: "type",
+                value: contract.type,
+            },
+            {
+                name: "team",
+                value: contract.team,
+            },
+            {
+                name: "created_at",
+                value: contract.created_at,
+            },
+            {
+                name: "updated_at",
+                value: contract.updated_at,
+            }
+        ],
     };
-    await datastore.save(contract);
+    await datastore.save(entity);
+    res.json(contract);
 });
 
 contractRegistryRouter.put("/version/new", authenticatedUser, async (req: Request, res: Response) => {
     const datastore = getDatastore();
-    const key = datastore.key("ContractRegistry");
-    const contract = {
+    const contract_id = req.body.contract_id;
+    const version = req.body.version;
+    const contractKey = datastore.key([CONTRACT_TABLE, req.body.contract_id]);
+    const [contract] = await datastore.get(contractKey);
+    if (!contract) {
+        return res.status(404).json({ error: "Contract not found" });
+    }
+    if (!contract_id || !version) {
+        return res.status(400).json({ error: "Contract ID and version are required" });
+    }
+    const key = datastore.key([CONTRACT_VERSION_TABLE, req.body.contract_id, req.body.version]);
+    const contractVersion = {
         key,
-        data: req.body,
+        data: [
+            {
+                name: "contract_id",
+                value: req.body.contract_id,
+            },
+            {
+                name: "version",
+                value: req.body.version,
+            },
+            {
+                name: "properties",
+                value: req.body.properties,
+                excludeFromIndexes: true,
+            },
+            {
+                name: "created_at",
+                value: new Date(),
+            },
+            {
+                name: "updated_at",
+                value: new Date(),
+            },
+        ]
     };
-    await datastore.save(contract);
+    if (contract.latest_version) {
+        contractVersion.data.push({
+            name: "previous_version",
+            value: contract.latest_version,
+        });
+    }
+    await datastore.save(contractVersion);
+    contract.latest_version = req.body.version;
+    contract.updated_at = new Date();
+    await datastore.update(contract);
+    res.json(contractVersion);
 });
 
 contractRegistryRouter.put("/instance/new", authenticatedUser, async (req: Request, res: Response) => {
     const datastore = getDatastore();
-    const key = datastore.key("ContractRegistry");
+    const key = datastore.key([CONTRACT_INSTANCE_TABLE, req.body.contract_id, req.body.version, req.body.network]);
     const contract = {
         key,
-        data: req.body,
+        data: [
+            {
+                name: "contract_id",
+                value: req.body.contract_id,
+            },
+            {
+                name: "version",
+                value: req.body.version,
+            },
+            {
+                name: "deployed_address",
+                value: req.body.deployed_address,
+            },
+            {
+                name: "network",
+                value: req.body.network,
+            },
+            {
+                name: "created_at",
+                value: new Date(),
+            },
+            {
+                name: "updated_at",
+                value: new Date(),
+            },
+        ]
     };
     await datastore.save(contract);
 });
--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -1,7 +1,7 @@
 import crypto from 'crypto';
 
-export const getId = (val: string): string => {
-    return crypto.createHash('sha256').update(val).digest('hex');
+export const getId = (val: string, len: number=10): string => {
+    return crypto.createHash('sha256').update(val).digest('hex').substring(0, len);
 }
 
 export const randomId = (len: number = 16): string => {
