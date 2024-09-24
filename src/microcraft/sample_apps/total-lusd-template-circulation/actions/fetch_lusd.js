3b2567453c218697c49be4d943d707b55ab26aaa
rohitbharti279 <rohitbharti279@gmail.com>
2024-09-24 16:21:30 +0530

    Human Readable ERC20 Balance

--- src/microcraft/sample_apps/total-lusd-template-circulation/actions/fetch_lusd.js
+++ src/microcraft/sample_apps/total-lusd-template-circulation/actions/fetch_lusd.js
@@ -17,20 +17,26 @@ async function fetchERC20Data() {
         const totalSupply = await contract.methods.totalSupply().call();
         const formattedTotalSupply = mcLib.web3.utils.fromWei(totalSupply, 'ether');
 
+        // Format the total supply in a human-readable format with commas
+        const humanReadableTotalSupply = new Intl.NumberFormat('en-US', {
+            minimumFractionDigits: 2,
+            maximumFractionDigits: 2,
+        }).format(formattedTotalSupply);
+
         // Fetch token name and symbol
         const tokenName = await contract.methods.name().call();
         const tokenSymbol = await contract.methods.symbol().call();
 
         // Update UI with the fetched data
-        data.erc20TotalSupply = formattedTotalSupply;
+        data.erc20TotalSupply = humanReadableTotalSupply;
         data.erc20Name = tokenName;
         data.erc20Symbol = tokenSymbol;
 
-        console.log(`Total ${tokenSymbol} Supply: ${formattedTotalSupply}`);
+        console.log(`Total ${tokenSymbol} Supply: ${humanReadableTotalSupply}`);
         console.log(`${tokenSymbol} Token Name: ${tokenName}`);
 
         return {
-            erc20TotalSupply: formattedTotalSupply,
+            erc20TotalSupply: humanReadableTotalSupply,
             erc20Name: tokenName,
             erc20Symbol: tokenSymbol
         };
