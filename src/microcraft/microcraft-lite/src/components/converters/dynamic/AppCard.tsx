59aa4693557670aa1d4bf85744f54186827cfc9f
rohitbharti279 <rohitbharti279@gmail.com>
2024-12-23 14:16:46 +0530

    add title and fixed width $ height

--- src/microcraft/microcraft-lite/src/components/converters/dynamic/AppCard.tsx
+++ src/microcraft/microcraft-lite/src/components/converters/dynamic/AppCard.tsx
@@ -15,8 +15,9 @@ const AppCard = ({
 }) => {
   return (
     <div
-      className="bg-white p-4 w-80 h-32 rounded-lg shadow-lg transform transition-all duration-300 hover:scale-105 hover:shadow-xl cursor-pointer relative"
+      className="bg-white p-4 w-80 h-24 rounded-lg shadow-lg transform transition-all duration-300 hover:scale-105 hover:shadow-xl cursor-pointer relative"
       onClick={() => onSelected(index)}
+      title={description}
     >
       <div className="flex flex-col items-center">
         {icon && (
@@ -27,14 +28,17 @@ const AppCard = ({
           />
         )}
         <h3 className="text-xl font-semibold text-center text-gray-800 mb-2">{title}</h3>
-        <p className="text-gray-500 text-sm text-center line-clamp-2 hover:line-clamp-none overflow-hidden transition-all duration-300">
+        <p className="text-gray-500 text-sm text-center line-clamp-2 overflow-hidden transition-all duration-300">
+          {description}
+        </p>
+        {/* <p className="text-gray-500 text-sm text-center line-clamp-2 hover:line-clamp-none overflow-hidden transition-all duration-300">
           {description}
         </p>
         <div className="absolute inset-0 bg-white opacity-0 hover:opacity-100 transition-opacity duration-300 overflow-hidden">
           <div className="h-full overflow-y-auto p-2">
             <p className="text-gray-600">{description}</p>
           </div>
-        </div>
+        </div> */}
       </div>
     </div>
   );
